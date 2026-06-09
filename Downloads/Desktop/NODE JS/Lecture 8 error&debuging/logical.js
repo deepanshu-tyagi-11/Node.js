9fa824e7a97d154dbb40d044b0586a6d1b7f4456
deepanshu-tyagi-11 <deepanshutyagi3211@gmail.com>
2026-06-09 13:28:27 +0530

    change

---
+++ Downloads/Desktop/NODE JS/Lecture 8 error&debuging/logical.js
@@ -0,0 +1,13 @@
+const testingSyntax=()=>{
+console.log("i am insde testing syntax");
+}
+const logical = () => {
+  let num = 5;
+  if (num = 10) {
+    console.log(num);
+  } else {
+    console.log("num is not 10");
+  }
+}
+
+module.exports = logical;
