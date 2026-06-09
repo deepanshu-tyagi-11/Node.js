ac3e2d0a5777798df550178a30a08374f637e087
deepanshu-tyagi-11 <deepanshutyagi3211@gmail.com>
2026-06-09 22:02:52 +0530

    add

---
+++ Downloads/Desktop/JAVA SCRIPT/LEVEL1/java script.js
@@ -0,0 +1,55 @@
+// [JS IN CONSOL].
+
+// .)mtlb hum apni java script ko direct insepct kr ka CONSOL ka ander chla sakta hai.
+
+// 1.) js in console(ALERT).
+// SYNTAX=alert('kuch bhe likh do ander').
+// console ka ander alert tag ka bhe use kr sakta hai syntax dusra.
+// 2.) js in console (math).
+// console ka ander maths ka solution bhe kr sakta hai.
+
+// [DOM].
+
+// .) apna pura webpage ka ek model /(ya object) bna lia jata hai  isa bolta ha DOM OBJECT..
+// =>document.body.innerHTML='pura page is called".
+// =>yadi ya command likhyga to console pai too pura page pai jo hum command kai ander likhyga vo he likh jayga .
+// =>) is command ka mtlb hai ke humna apna pura webpage ko modify kr dia aur apna message pass kr dia .[pura page is called].
+
+// .)document.body.innerHTML='<b> pura page is called </b>'.
+//  => is syntax ke madad se hum html bhe chnage kr skata hai.
+
+//  .)document.getElementsByTagName('b')[0].style.fontSize='44px'//RE CAP
+//  => is syntax ke madad se hum apni css bhe change kr sakta hai.
+
+ .)document.getElementsByClassName('gNO89b')[1].click().
+//  =>hum js se action bhe perform kr sakta hai using .
+//  => jis bhe cheej ko chalena ho apna kis page pai without using toch too hum is ke [class] ko  copy kraga insepct kr kai aur console mai ja ka paste ke dega. to chal jayga using syntax jo upper line mai likha hai. -->
+
+// NOTE => JS se hum html css aur js teeno cheeja  chnage kr sakta hai console ka ander .
+
+// [CHROME EXTENSIONS]...
+// .)jsha hamara playstore hota hai asha he humara chrome ka ander chorme ke  extension hoti hai . 
+// .) aDd new funclanoties in chrome.
+// .)jsha hamara khai pai add ata hai to hum chrome extension ke madad se usha hata sakta hai console ka ander he js ko chnage kr ka.
+
+// [SYNTAX].
+// =>structure of word in a sentence.
+// =>rule of the language.
+// => alert('hello keshav don') -->
+
+//  [BROWSER]..
+// browser jo hota hai vo server ka pass jata hai aur html css js ke files ko  download kr leta hai. Aur phir downloaded files ko execute kr leta hai aur is cheej ko bolta hai rendring ..phir hama browser  accha se webpage pai dikha deta hai..
+
+// [JS].
+// => enable interactvity.
+// => respond to user action. user ka action ko respond krna.
+// => data fetch and send information to server.
+// => java script run at the client side of the browser.
+// =>js ka code ko  hum [coffe script / type script ]mai bhe lih sakta hai pr hum TRANSPILE ke madad se code ko js mai he change kr leta hai.
+
+// .)transpile mai code ko ek programmimg labguage ko dusri programming languge mai convert kr dia.
+// =>jo marji kis bhe language mai code likho pr TRANSPILE usha js mai convert kr ka chla deta hai.
+           [SYNTAX]
+(x)=> x+1 ----TRANSPILE-  function(x){ 
+                              return(x+1);
+                               }
