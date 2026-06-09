fde450a57af0c9d3304fb633f80258fa58a3e1ab
deepanshu-tyagi-11 <deepanshutyagi3211@gmail.com>
2026-06-09 22:14:29 +0530

    add second

---
+++ Downloads/Desktop/JAVA SCRIPT/LEVEL 2/operation.js
@@ -0,0 +1,33 @@
+//       [NUMBERS AND STRING]
+
+// 1.)ARTHMETICS OPERATOR.
+
+// =>+ ,-,/,%,*
+// there are some opertaors .
+// example=>((2499-(10/100*2499))-29+(15599-(41/100*15599))-272+34+25)*118/100.
+// =>haimna yaha pai myntra ka to order ke calculation kri hai discount vegra laga kai.using operators.
+// => lengthy calculation in dono rule ke madad se kr sakata hai..[ BODMAS=> BRACKET, OPEN , DIVIDE,MULTIPLY, ADD, SUBSTRACT OR PEMDSA=> PARANTHESIIS ,EXPONENT, MULTIPLY, DIVIDE,SUBSTRACT, ADD].
+// => isma kya hai ke hama accha se bracket ka istmal krna ha. phir hamri js accha se calculation krage ..
+
+// 2.) [TYPES OF NUMBER]
+
+// i.)INTEGER.
+// =>
+
+// ii.)FLOAT.  
+
+// =>basically hama floating calculation se bachna chyia.
+// => always do calculatio in paisa INSTEAD of rupees.
+// => yadi hama rupee ke  calculation krni hai to hamesha phla hum paisa mai covert krayga phir calcuaton krayga..
+// =>yadi floating number ko rounded krna ho to hum[MATH.ROUND] ka use kr sakta hai.
+// ex=> Math.round(1824.6345) asha..
+
+// 3.)DONT LEARN SYNTAX..
+
+// => kuch  bhe yad nhi krna ha bs sabhi concept samjhna hai.kuch bhe cheej dkhni ho in website pai jaa ka smj sakta hai.
+// => UNDERSTAND THE CONCEPT NOT JUST SYNTAX.
+
+           
+// . CHAT GPT
+// . GOOGE
+// .DEVELOPER.MOZILLARA.COM
